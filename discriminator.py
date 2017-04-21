daaa38e0b1d541fc1cc69938099cbe42c6dc6e80
Shalom Yiblet <shalom.yiblet@gmail.com>
2017-04-21 12:54:37 -0400

    removed maxpooling form discriminator

--- discriminator.py
+++ discriminator.py
@@ -43,7 +43,7 @@ def conv_net(x):
         'c4': filters(256, 512),
         'c5': filters(512, 512),
         'c6': filters(512, 512),
-        'd1': denseW(5*5*512, 1),
+        'd1': denseW(5*5*512, 1),  # TODO check this dimension
     }
 
     biases = {
@@ -59,22 +59,16 @@ def conv_net(x):
     x = tf.reshape(x, shape=[-1, 128, 128, 1])
 
     c1 = conv(x, weights['c1'], biases['c1'], strides=2)
-    c1 = maxpool(c1)
 
     c2 = conv(c1, weights['c2'], biases['c2'], strides=2)
-    c2 = maxpool(c2)
 
     c3 = conv(c2, weights['c3'], biases['c3'], strides=2)
-    c3 = maxpool(c3)
 
     c4 = conv(c3, weights['c4'], biases['c4'], strides=2)
-    c4 = maxpool(c4)
 
     c5 = conv(c4, weights['c5'], biases['c5'], strides=2)
-    c5 = maxpool(c5)
 
     c6 = conv(c5, weights['c6'], biases['c6'], strides=2)
-    c6 = maxpool(c6)
 
     d1 = dense(c6, weights['d1'], biases['d1'])
 
