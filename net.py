bc0798902a5471ec16a7e66d1494743bbe54ed1e
BENGALIBALA\Irina <aluepageelover@gmail.com>
2017-04-21 15:01:12 -0400

    Added various placeholders.

--- net.py
+++ net.py
@@ -5,6 +5,20 @@ import numpy as np
 from generator import u_net
 from discriminator import conv_net
 
+minib_size = 128
+X_dim = 128*128
+y_dim = 128*128
+Z_dim = 100
+
+X = tf.placeholder(tf.float32, shape=[None, X_dim])
+y = tf.placeholder(tf.float32, shape=[None, y_dim])
+Z = tf.placeholder(tf.float32, shape=[None, Z_dim])
+
+""" Discriminator """
+D_theta = []
+
+""" Generator """
+G_theta = []
 
 def generator(x):
     return u_net(x)
@@ -29,14 +43,13 @@ D_loss = -tf.reduce_mean(tf.log(D_real) + tf.log(1. - D_fake))
 G_loss = -tf.reduce_mean(tf.log(D_fake)) + tf.reduce_mean(X - D_fake)
 
 # Apply an optimizer here to minimize the above loss functions
-D_solver = tf.train.AdamOptimizer().minimize(D_loss) # --> add var_list
-G_solver = tf.train.AdamOptimizer().minimize(G_loss) # --> add var_list
+D_solver = tf.train.AdamOptimizer().minimize(D_loss, var_list = D_theta) 
+G_solver = tf.train.AdamOptimizer().minimize(G_loss, var_list = G_theta) 
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 epochs = 10000
-minib_size = 128
 
 for i in range(epochs):
 
