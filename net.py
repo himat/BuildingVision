b766097cb9a237a99456d9ee2a92935dfe7c1367
Hima <himat15@yahoo.com>
2017-04-23 17:14:18 -0400

    Batching now keeps ground truth images in sync with edges images

--- net.py
+++ net.py
@@ -6,7 +6,16 @@ import matplotlib.pyplot as plt
 from generator import u_net
 from discriminator import conv_net, conv_weights
 
-minib_size = 128
+epochs = 10
+mb_size = 14
+
+train_path = "/sample_data" #"data/"
+test_path = "/data/*****"
+
+IMAGE_DIM = 128
+IMAGE_SIZE = 16384 # 128 x 128
+input_nc = 3 # number of input image channels
+
 X_dim = 128*128
 y_dim = 128*128
 Z_dim = 100
@@ -21,16 +30,6 @@ D_theta = []
 """ Generator """
 G_theta = []
 
-epochs = 10
-minib_size = 14
-
-IMAGE_DIM = 128
-IMAGE_SIZE = 16384 # 128 x 128
-input_nc = 3 # number of input image channels
-
-train_path = "/sample_data" #"data/"
-test_path = "data/**"
-
 def generator(x):
     return u_net(x)
 
@@ -44,31 +43,67 @@ def next_data_batch(minibatch_size):
     pass
 
 dir = os.path.dirname(os.path.realpath(__file__))
-input_folder = dir + train_path + "/*.jpg"
 
-filenames = tf.train.match_filenames_once(input_folder)
-filename_queue = tf.train.string_input_producer(filenames)
+filetype = ".jpg"
+ground_truth_files_path = dir + train_path + "/ground_truth"
+ground_truth_files = ground_truth_files_path + "/*" + filetype
+edges_files_path = dir + train_path + "/edges" 
+edges_files = edges_files_path + "/*" + filetype 
 
-image_reader = tf.WholeFileReader()
-key_fname, value_imgfile = image_reader.read(filename_queue)
-print "file: ", key_fname
+truth_filenames_tf = tf.train.match_filenames_once(ground_truth_files)
+
+truth_filenames_np = []
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    truth_filenames_np = sess.run(truth_filenames_tf)
+
+np.random.shuffle(truth_filenames_np)
+truth_filenames_tf = tf.convert_to_tensor(truth_filenames_np)
+
+def get_edges_file(f):
+    # Splits at last occurrence of / to get the file name
+    actual_file_name = f.rpartition("/")[2]
+    return edges_files_path + "/" + actual_file_name
+
+edges_fnames = [get_edges_file(f) for f in truth_filenames_np]
+edges_fnames_tf = tf.convert_to_tensor(edges_fnames)
+
+print "Truth list shape: ", truth_filenames_tf.shape
+print "Edges list shape: ", edges_fnames_tf.shape
+
+print "truth filenames: ", truth_filenames_np
+print "edges filenames: ", edges_fnames
+
+
+truth_image_name, edges_image_name = tf.train.slice_input_producer([truth_filenames_tf, edges_fnames_tf], shuffle=False)
+
+value_truth_imgfile = tf.read_file(truth_image_name)
 
 # Returns as a tensor for us to use 
-image = tf.image.decode_jpeg(value_imgfile)
+truth_image = tf.image.decode_jpeg(value_truth_imgfile)
+truth_image.set_shape([IMAGE_DIM, IMAGE_DIM, input_nc])
 # image = tf.reshape(image, [IMAGE_SIZE*input_nc])
-image = tf.cast(image, tf.float32)
-image = image/255.0 # Normalize RGB to [0,1]
-print "image: ", type(image)
+truth_image = tf.cast(truth_image, tf.float32)
+truth_image = truth_image/255.0 # Normalize RGB to [0,1]
+
+
+value_edges_imgfile = tf.read_file(edges_image_name)
+edges_image = tf.image.decode_jpeg(value_edges_imgfile)
+edges_image.set_shape([IMAGE_DIM, IMAGE_DIM, 1])
+# edges_image = tf.reshape(edges_image, [IMAGE_DIM, IMAGE_DIM])
+# edges_image = edges_image.squeeze() # Get rid of single channel third dim
+edges_image = tf.cast(edges_image, tf.float32)
+edges_image = edges_image/255.0
 
 min_queue_examples = mb_size 
 
 # Background thread to batch images
 images_batch = tf.train.shuffle_batch(
-                [image],  #image_tensor
+                [truth_image, edges_image],  #image_tensor
                 batch_size=mb_size,
                 capacity=min_queue_examples + 2*mb_size,
                 min_after_dequeue=min_queue_examples,
-                shapes=([IMAGE_DIM, IMAGE_DIM, input_nc]),
+                # shapes=([IMAGE_DIM, IMAGE_DIM, input_nc]),
                 num_threads=4,
                 allow_smaller_final_batch=True)
 
@@ -108,18 +143,17 @@ with tf.Session() as sess:
 
     for i in range(epochs):
 
-	if i % 1000 == 0:
-	    print("Epoch ", i)
+    	if i % 1000 == 0:
+    	    print("Epoch ", i)
 
-        # Get next batch
-        X_sketches = images_batch.eval()
+            # Get next batch
+            [X_truth_batch, X_edges_batch] = sess.run([truth_images_batch, edges_images_batch])
 
-        X_ground_truth = 0 ## Figure out how to get sketches 
 
-	_, D_loss_curr = sess.run([D_solver, D_loss],
-                feed_dict={X_sketch: X_sketches, X_ground_truth: X_true})
-	_, G_loss_curr = sess.run([G_solver, G_loss],
-                feed_dict={X_ground_truth: X_true})
+    	_, D_loss_curr = sess.run([D_solver, D_loss],
+                    feed_dict={X_ground_truth: X_truth_batch, X_sketch: X_edges_batch })
+    	_, G_loss_curr = sess.run([G_solver, G_loss],
+                    feed_dict={X_ground_truth: X_truth_batch})
 
     # Stops background threads
     coord.request_stop()
