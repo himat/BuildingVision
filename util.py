ece82df3d38db8c17a952ddfc01f6d2991733d61
BENGALIBALA\Irina <aluepageelover@gmail.com>
2017-04-24 17:44:51 -0400

    ignore

--- util.py
+++ util.py
@@ -10,18 +10,18 @@ output_dir = "out"
 def plot_single(image):
         if image.shape[2] == 1:
             image = image.squeeze()
-	plt.imshow(image, cmap="Greys_r")
-	plt.show() 
+    	plt.imshow(image, cmap="Greys_r")
+    	plt.show() 
 
 # Plots a batch of images and optionally saves to disk
 def plot_save_batch(batch, iter_num, save_only=False):
-        (mb_size, width, height, channels) = batch.shape
+    (mb_size, width, height, channels) = batch.shape
 
-        sqrt = math.sqrt(mb_size)
-        if sqrt.is_integer():
-            fig_shape = (int(sqrt), int(sqrt))
-        else:
-            fig_shape = (1, mb_size)
+    sqrt = math.sqrt(mb_size)
+    if sqrt.is_integer():
+        fig_shape = (int(sqrt), int(sqrt))
+    else:
+        fig_shape = (1, mb_size)
 
 	fig = plt.figure(figsize=fig_shape)
 	gs = gridspec.GridSpec(*fig_shape)
@@ -37,10 +37,10 @@ def plot_save_batch(batch, iter_num, save_only=False):
 		if sample.shape[2] == 1:
 			sample = sample.squeeze()
 
-		plt.imshow(sample, cmap="Greys_r")
+	   plt.imshow(sample, cmap="Greys_r")
 
-        if save_only:        
-            plt.savefig(os.path.join(output_dir, str(iter_num).zfill(3)+".png"), bbox_inches='tight')
-            plt.close(fig)
-        else:
-	    plt.show() 
+    if save_only:        
+        plt.savefig(os.path.join(output_dir, str(iter_num).zfill(3)+".png"), bbox_inches='tight')
+        plt.close(fig)
+    else:
+        plt.show() 
